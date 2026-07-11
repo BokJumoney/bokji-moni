58bc6bcd7415bc97209e4c307de8977240c03122
minho2618 <minho2618@naver.com>
2026-07-11 14:46:18 +0900

    Feat: env 파일 읽어오기 설정

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -1,1 +1,13 @@
 # 환경 변수 (Pydantic Settings)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "edudb"
+    DB_USER: str = "edu"
+    DB_PASSWORD: int = 1234
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings()
