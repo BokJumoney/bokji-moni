52d649ccbd23eaa722b8c21f5fc2baabddf81d20
minho2618 <minho2618@naver.com>
2026-07-16 17:37:35 +0900

    Feat: 전체 랭그래프에 subscription_graph 추가

--- app/domain/chat/graph/chat_graph.py
+++ app/domain/chat/graph/chat_graph.py
@@ -7,25 +7,29 @@ from app.domain.chat.graph.nodes.casual_talk import casual_talk
 from app.domain.chat.graph.router.conversatioin_router import check_conversation_mode
 from app.domain.chat.graph.nodes.application_entry import application_entry
 from app.domain.chat.graph.nodes.intent_router import intent_router
+from app.domain.chat.graph.subgraph.subscription_graph import subscription_graph
 #====메인 그래프. 일반대화와 신청보조를 연결하는 교차로====
-
 
 # 이 그래프는 ChatGraphState 사용
 workflow = StateGraph(ChatGraphState)
-#노드 등록 
+
+# 노드 등록
 workflow.add_node("retrieve", retrieve)
 workflow.add_node("generate", generate)
 workflow.add_node("casual_talk", casual_talk)
 workflow.add_node("application_graph", application_graph)
 workflow.add_node("application_entry", application_entry)
 workflow.add_node("intent_router",intent_router)
-#조건 분기 
+workflow.add_node("subscription_graph", subscription_graph)
+
+# 조건 분기
 workflow.add_conditional_edges(
     START,
     check_conversation_mode,
     {
         "general":"intent_router",
-        "application":"application_entry"
+        "application":"application_entry",
+        "subscription":"subscription_graph",
     }
 )
 workflow.add_conditional_edges(
@@ -34,7 +38,8 @@ workflow.add_conditional_edges(
     {
         "vectorstore":"retrieve",
         "casual_talk":"casual_talk",
-        "application":"application_entry"
+        "application":"application_entry",
+        "subscription":"subscription_graph",
     }
 )
 # 추가
@@ -46,5 +51,6 @@ workflow.add_edge(
 workflow.add_edge("retrieve","generate")
 workflow.add_edge("generate",END)
 workflow.add_edge("casual_talk",END)
+workflow.add_edge("subscription_graph", END)
 
 graph = workflow.compile()
