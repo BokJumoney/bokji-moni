a8058cfe07242b9619993ef1c168b037fcffd8ed
minho2618 <minho2618@naver.com>
2026-07-16 18:26:16 +0900

    Feat: State 수정

--- app/domain/chat/graph/state.py
+++ app/domain/chat/graph/state.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import Annotated, List
 from typing_extensions import TypedDict
 
 from langchain_core.documents import Document
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 
 #LangGraph에서 공유하는 상태 객체
 # 각 노드는 직접 값을 주고받지 않고 State를 통해 데이터를 공유함
@@ -24,3 +26,5 @@ class ChatGraphState(TypedDict, total=False):
     policy_query: str
     candidate_policy_ids: list[int]
     selected_policy_id: int
+    # ToolNode가 AIMessage와 ToolMessage를 같은 요청 안에서 이어 붙일 때 사용한다.
+    messages: Annotated[List[BaseMessage], add_messages]
